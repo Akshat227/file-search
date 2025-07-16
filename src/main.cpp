e8506cf0978285cb7fc6540a54321ead27d72e15
akshat227 <akshatkhare364@gmail.com>
2025-07-16 12:34:02 +0530

    nothing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 
 namespace fs = std::filesystem;
 
@@ -8,12 +10,25 @@ int main(int argc, char const *argv[])
 {
     std::string path = "/home/akshat";
     std::ofstream outFile("pathdata.txt"); // Opens file for writing (overwrites if exists)
-
+    std::vector<std::string> files;
     for (const auto &entry : fs::recursive_directory_iterator(path))
     {
-        outFile << entry.path() << "\n";
+        std::string filepath = entry.path().string();
+        files.push_back(filepath);
+        outFile << filepath << "\n";
     }
+    std::string target = "cmake";
     outFile.close();
-    std::cout << "Data written to my_output.txt successfully." << std::endl;
+    auto it = std::find(files.begin(), files.end(), target);
+
+    if (it != files.end())
+    {
+        std::cout << "✅ Found: " << *it << "\n";
+    }
+    else
+    {
+        std::cout << "❌ Not found: " << target << "\n";
+    }
+
     return 0;
 }
